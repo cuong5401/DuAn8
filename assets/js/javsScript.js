5010ffc3c6e12ef7c72b0c20166868532d25d3fd
レ カオ クオン <lc_cuong@ad01.corp.toadenshi.co.jp>
2025-11-27 14:04:54 +0900

    done navbar on pc

--- assets/js/javsScript.js
+++ assets/js/javsScript.js
@@ -133,3 +133,45 @@ function handleActiveMenu() {
         dropdown.onmouseleave = () => init();
     });
 }
+
+
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * <button class="js-toggle" toggle-target="#box">Click</button>
+ * <div id="box">Content show/hide</div>
+ */
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+    $$(".js-toggle").forEach((button) => {
+        const target = button.getAttribute("toggle-target");
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains("hide");
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle("hide", !isHidden);
+                $(target).classList.toggle("show", isHidden);
+            });
+        };
+    });
+}
+
+
+// đóng lại mỗi lần chuyển sang size màn lớn hơn 
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 991.8) {
+        const el = document.querySelector(".navbar.show");
+        if (el) {
+            el.classList.remove("show");
+            el.classList.add("hide");
+        }
+    }
+});
